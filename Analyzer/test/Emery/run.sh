bc243d1c1d23a96c6eb553c5078c8b94f7720b74
Emery Nibigira <emery.nibigira@cern.ch>
2024-06-27 23:14:11 +0200

    update script

--- Analyzer/test/Emery/run.sh
+++ Analyzer/test/Emery/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-infile=file:/eos/user/e/enibigir/data/HSCP/store/mc/RunIISummer20UL18RECO/HSCPgluino_M-1800_TuneCP5_13TeV-pythia8/AODSIM/106X_upgrade2018_realistic_v11_L1v1-v2/80000/236673F3-65E3-A745-AEFE-DBED7237EDF1.root
+infile=file:236673F3-65E3-A745-AEFE-DBED7237EDF1.root
 outfile=HSCPgluino_M-1800_fromAOD_ref.root
 
-
+if [ ! -f 236673F3-65E3-A745-AEFE-DBED7237EDF1.root ]; then
+	scp enibigir@lxplus.cern.ch:/eos/user/e/enibigir/data/HSCP/store/mc/RunIISummer20UL18RECO/HSCPgluino_M-1800_TuneCP5_13TeV-pythia8/AODSIM/106X_upgrade2018_realistic_v11_L1v1-v2/80000/236673F3-65E3-A745-AEFE-DBED7237EDF1.root .
+fi
 cmsRun HSCParticleProducerAnalyzer_cfg.py inputFiles=$infile outputFile=$outfile maxEvents=$1
