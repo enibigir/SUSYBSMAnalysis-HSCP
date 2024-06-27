0671314c71de8fc78ea25a4feb277771f3b4b370
Emery Nibigira <emery.nibigira@cern.ch>
2024-06-27 22:39:10 +0200

    get calib files

---
+++ Analyzer/test/Emery/fresh_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/Analyzer/test/Emery/HSCParticleProducerAnalyzer_cfg.py .
+cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/HSCP/data/template_2018MC_v5.root .
+cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/HSCP/data/MuonTimeOffset.txt .
