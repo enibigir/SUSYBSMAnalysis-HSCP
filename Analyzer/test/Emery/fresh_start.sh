a80ebd6fad0100e2fe9ca391c98c0c9d56a09e84
Emery Nibigira <emery.nibigira@cern.ch>
2024-07-11 08:30:07 +0200

    update script

--- Analyzer/test/Emery/fresh_start.sh
+++ Analyzer/test/Emery/fresh_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/Analyzer/test/Emery/HSCParticleProducerAnalyzer_cfg.py .
+if [ ! -f HSCParticleProducerAnalyzer_cfg.py ]; then
+    cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/Analyzer/test/Emery/HSCParticleProducerAnalyzer_cfg.py .
+fi
 cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/HSCP/data/template_2018MC_v5.root .
 cp ${CMSSW_BASE}/src/SUSYBSMAnalysis/HSCP/data/MuonTimeOffset.txt .
